e548d207a576ea0f14e56f2411ee86231de5349a
tucker <tuckerwray84@gmail.com>
2020-10-25 00:02:15 -0400

    Initial setup
    creates Class Components to house the basic structure of getting started tutorial tic tac toe game win React.js
    
     - Game
     - Board
     - Square
    - Borrows default index.css styles

---
+++ tic-tac-toe/src/Board.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Square from './Square'
+export default class Board extends Component {
+  static propTypes = {
+    prop: PropTypes,
+  };
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ tic-tac-toe/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Game from "./Game";
+function App() {
+  return <Game />;
+}
+
+ReactDOM.render(
+  <App />,
+   document.getElementById("root")
+   );
