67c25cc09acda8463e7409e42605f297c95d9d87
tucker <tuckerwray84@gmail.com>
2020-10-29 15:23:44 -0400

    ## ErrorBoundary  - created React Core suggested ErrorBoundary to trace buggy state and purge broken components from the tree

---
+++ tic-tac-toe/src/errorBoundary.js
@@ -0,0 +1,30 @@
+import React,{Component} from 'react'
+
+export default class ErrorBoundary extends Component{
+
+
+    constructor(props){super(props)
+        this.state={error:null,errorInfo:null}
+    }
+    
+    
+    render(){
+        
+        
+        if( this.state.errorInfo){
+            return(
+                <>
+                    <div>this following error occurred</div>
+                    <p>error: 
+                    {this.state.error.toString()}
+                    </p>
+                    
+                    <p>at this location:
+                    {this.state.errorInfo.stack}            
+                    </p>
+                </>
+                )
+                
+            }else{ return( this.props.children )}
+}
+}
