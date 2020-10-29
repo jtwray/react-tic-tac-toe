83dca0c8929a743596462b5f93fd551a94e9b088
tucker <tuckerwray84@gmail.com>
2020-10-29 19:37:27 -0400

    ## Error Boundary  Main App COmponent wrapped

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Game from "./Game";
+import ErrorBoundary from "./errorBoundary";
 function App() {
-  return <Game />;
+  return (
+    <ErrorBoundary>
+      <Game />
+    </ErrorBoundary>
+  );
 }
 
-ReactDOM.render(
-  <App />,
-   document.getElementById("root")
-   );
+ReactDOM.render(<App />, document.getElementById("root"));
