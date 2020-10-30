9c276cd75c89fb6dd9bfaccee41e64209edc3007
tucker <tuckerwray84@gmail.com>
2020-10-30 18:37:08 -0400

    ## Syntax Formatting  - code readibility improved with syntax formatting

--- tic-tac-toe/src/Board.js
+++ tic-tac-toe/src/Board.js
@@ -1,32 +1,38 @@
 import React, { Component } from "react";
 import ErrorBoundary from "./errorBoundary";
-import Square from './Square'
+import Square from "./Square";
 export default class Board extends Component {
   callWinner = (squares) => {
-    const winningCombos =
-      [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-      ]
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
     if (this.props.currentMoveIndex > 3) {
-//  loop through the winning combinations fro three in a row
+      //  loop through the winning combinations fro three in a row
       for (let [i] in winningCombos) {
-//  assign variables to the 1st 2nd and 3rd in a row for a combination
+        //  assign variables to the 1st 2nd and 3rd in a row for a combination
         const [a, b, c] = winningCombos[i];
-// if a ,b ,c are all 'x' return 'x'  
-// if a ,b ,c are all 'o' return 'o'
-        if (squares[a] != null && squares[a] === squares[b] && squares[a] === squares[c]) { return squares[a] }
-// else return null aka falsey
-      } return null;
+        // if a ,b ,c are all 'x' return 'x'
+        // if a ,b ,c are all 'o' return 'o'
+        if (
+          squares[a] != null &&
+          squares[a] === squares[b] &&
+          squares[a] === squares[c]
+        ) {
+          return squares[a];
+        }
+        // else return null aka falsey
+      }
+      return null;
     }
-  }
+  };
 
   renderSquare(i) {
     return (
@@ -40,20 +46,20 @@ export default class Board extends Component {
   }
 
   render() {
-
     let status = `Next player:${this.props.nextMoveIs_X ? "X" : "O"} `;
     const winner = this.callWinner(this.props.squares);
-    if (winner) { status = `Winner:${winner}` }
+    if (winner) {
+      status = `Winner:${winner}`;
+    }
 
     return (
-      <div style={{minWidth:"50%vw",border:"solid orange 1px"}}>
+      <div style={{ minWidth: "50%vw", border: "solid orange 1px" }}>
         <ErrorBoundary>
           <div className="status">Move# {this.props.currentMoveIndex}</div>
           <div className="status">{status}</div>
-
         </ErrorBoundary>
         <ErrorBoundary>
-          <div className="board-row" style={{minWidth:"100%"}}>
+          <div className="board-row" style={{ minWidth: "100%" }}>
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
--- tic-tac-toe/src/Square.js
+++ tic-tac-toe/src/Square.js
@@ -1,53 +1,51 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 export default class Square extends Component {
-
-
-    handleOnMouse() { }
-    render() {
-        return (
-            <button
-                onMouseOver={(e) => {
-                    e.target.style.background = 'darkred';
-                    e.target.style.color = "darkred";
-                    e.target.style.textShadow = `
+  handleOnMouse() {}
+  render() {
+    return (
+      <button
+        onMouseOver={(e) => {
+          e.target.style.background = "darkred";
+          e.target.style.color = "darkred";
+          e.target.style.textShadow = `
                     .4px .4px 1px white,
                     -.5px -.5px .2px black,
                     1px .5px .2px black
-                    `
-                }}
-                onMouseLeave={(e) => {
-                    e.target.style.color = "darkred";
-                    e.target.style.textShadow = `
+                    `;
+        }}
+        onMouseLeave={(e) => {
+          e.target.style.color = "darkred";
+          e.target.style.textShadow = `
                     -.4px -.4px .2px white,
                     .4px .4px .2px black
-                    `
-                }}
-
-                style={{
-                    transition: "all .4s ease",
-                    border: 'solid .125rem firebrick',
-                    borderBottom: ".2rem solid pink",
-                    borderTop: ".1rem solid red",
-                    borderLeft: ".1rem solid pink",
-                    background: "darkred",
-                    color: "darkred",
-                    textShadow: `-.4px -.4px .2px white,
+                    `;
+        }}
+        style={{
+          transition: "all .4s ease",
+          border: "solid .125rem firebrick",
+          borderBottom: ".2rem solid pink",
+          borderTop: ".1rem solid red",
+          borderLeft: ".1rem solid pink",
+          background: "darkred",
+          color: "darkred",
+          textShadow: `-.4px -.4px .2px white,
                     .4px .4px .2px black`,
-                    width: "10vw",
-                    wordWrap: "wrap",
-                    fontSize: '1.5rem',
-                    height: "10vw"
-                }}
-                className="square"
-
-                onClick={()=>this.props.onClick(this.props.value)} 
-                > { this.props.value} 
-                </button>)
-    }
+          width: "10vw",
+          wordWrap: "wrap",
+          fontSize: "1.5rem",
+          height: "10vw",
+        }}
+        className="square"
+        onClick={() => this.props.onClick(this.props.value)}
+      >
+        {" "}
+        {this.props.value}
+      </button>
+    );
+  }
 }
-
 
 Square.propTypes = {
-    value: PropTypes.number
-}
+  value: PropTypes.number,
+};
