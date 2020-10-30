f2e619f3bdf5cfd56ae9ff0a9a196c88b812e747
tucker <tuckerwray84@gmail.com>
2020-10-29 21:36:12 -0400

    ## handleClick-- Board.js Game.js - lifts state for handleClick to Game.js - removes constructor from Board.js - could refactor into a fuctional component

--- tic-tac-toe/src/Board.js
+++ tic-tac-toe/src/Board.js
@@ -1,55 +1,67 @@
 import React, { Component } from "react";
+import ErrorBoundary from "./errorBoundary";
 import Square from './Square'
 export default class Board extends Component {
-  state = {
-    squares: Array(9).fill(null),
-    nextMoveIs_X: true,
-  }
+  callWinner=(squares)=> {
+    const winningCombos =
+      [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ]
 
-  handleClick(i) {
-    const nextMoveSquares = [...this.state.squares];
-    nextMoveSquares[i] = this.state.nextMoveIs_X ? "X" : "O";
-    const prevMoves = [...this.props.moveHistory];
-    prevMoves.push(this.state.squares);
-    this.setState({
-      squares: nextMoveSquares,
-      moveHistory: prevMoves,
-      nextMoveIs_X: !this.state.nextMoveIs_X
-    });
+    if (this.props.currentMoveIndex > 3) {
+      for (let [i] in winningCombos) {
+        const [a, b, c] = winningCombos[i];
+        if (squares[a] != null && squares[a] === squares[b] && squares[a] === squares[c]) { return squares[a] }
+      } return null;
+    }
   }
 
   renderSquare(i) {
     return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
+      <ErrorBoundary>
+        <Square
+          value={this.props.squares[i]}
+          onClick={ ()=>this.props.handleMove(i)}
+        />
+      </ErrorBoundary>
     );
   }
 
   render() {
-    const status = "Next player: X";
-
+    let status = `Next player:${this.props.nextMoveIs_X ? "X" : "O"} `;
+    const winner = this.callWinner(this.props.squares);
+    if (winner) { status = `Winner:${winner}` }
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
+        <ErrorBoundary>
+          <div className="status">{status}</div>
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
 
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
 
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </ErrorBoundary>
       </div>
     );
   }
